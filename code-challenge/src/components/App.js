c731e9a853c631e8bddda7ef25b0810e29cdc076
DicksonLewis <dickylewiss@gmail.com>
2024-05-18 22:59:40 +0300

    adds files

---
+++ code-challenge/src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import BotsPage from "./BotsPage";
+
+
+function App() {
+  return (
+    <div className="App">
+      <BotsPage />
+     
+
+    </div>
+  );
+}
+
+export default App;
